feeb783695ec56663baa5d09ad2217b41de19004
Ryusei Ishida <ryuuseiishidachouchou2@gmail.com>
2021-01-18 02:28:08 +0900

    じゃんけん

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
-var alertString;
-alertString = addString("Webcamp");
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
 
-alert(alertString);
+ var js_hand = getJShand();
 
+ var judge = winlose(user_hand, js_hand);
 
-function addString(strA) {
-  var addStr = "Hello" + strA;
-  return addStr;
-}
+ alert('あなたの選んだ手は' + user_hand + 'です');
+
+ function getJShand() {
+   var js_hand_num = Math.floor( Math.random() * 3);
+   var hand_name;
+   if(js_hand_num == 0){
+     hand_name = "グー";
+   } else if(js_hand_num == 1){
+     hand_name = "チョキ";
+   } else if(js_hand_num == 2){
+     hand_name = "パー";
+   }
+   return hand_name; 
+ }
+
+ function winlose(user,js){
+   var winlosestr;
+
+   if(user == "グー"){
+     if(js == "グー"){
+       winlosestr = "あいこ";
+     } else if (js == "チョキ"){
+       winlosestr = "勝ち";
+     } else if(js =="パー"){
+       winlosestr = "負け";
+     }
+   }  else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+ }
